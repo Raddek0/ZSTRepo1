b6a179ee2166957fffdc9d3706b1ab487f42a6e9
Radosław <mojemail@>
2023-09-15 13:06:08 +0200

    Dodano ify

--- login.php
+++ login.php
@@ -3,4 +3,13 @@ DEFINE('cos','ahisjtnhgdjdg');
 DEFINE('cos2','ahisjtnhgdjdg');
 DEFINE('cos3','ahisjtnhgdjdg');
 DEFINE('cos4','ahisjtnhgdjdg');
+
+if(1>0)
+{
+    echo "nic";
+}
+else
+{
+    echo "dupa";
+}
 ?>
